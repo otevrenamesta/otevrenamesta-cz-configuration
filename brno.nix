fe6fe7fea16e49da20c828998235f7477e18b79f
System administrator <nesnera@email.cz>
2018-02-09 10:38:13 +0100

    update brno configuration

--- brno.nix
+++ brno.nix
@@ -20,10 +20,16 @@ in
     listenAddress = "127.0.0.1";
   };
 
+  boot.initrd.availableKernelModules = [ "ata_piix" "mptspi" "floppy" "sd_mod" "sr_mod" ];
+
+  users.extraUsers.root.hashedPassword = "$6$jcrgonWA$AREhvoz2Wq4kHidp32rESWVbJhHqVVBmiCCLUwnmA78N4Fwjy3LX9PZDw1WKtXhSrbA3BggQr1i7Df9FveYIz0";
+
+
   services.openssh.enable = true;
   services.openssh.challengeResponseAuthentication = true; #opet default
   services.openssh.passwordAuthentication = true; #default je true tj. melo by byt zbytecne
-  services.openssh.permitRootLogin = "without-password"; #"yes";
+  #services.openssh.permitRootLogin = "without-password";
+  services.openssh.permitRootLogin = "yes";
   services.openssh.ports = [ 22 ];
 
   networking.firewall.allowedTCPPorts = [ 443 ];
