216bfe622eaee249bbddd560d0c5869b982f45be
Richard Marko <srk@48.io>
2019-05-13 14:53:00 +0200

    add ct.nix

---
+++ ct.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, lib, ... }:
+
+let
+  vpsadminos = builtins.fetchTarball https://github.com/vpsfreecz/vpsadminos/archive/master.tar.gz;
+in
+{
+  imports = [
+    <nixpkgs/nixos/modules/virtualisation/container-config.nix>
+    "${vpsadminos}/os/lib/nixos-container/build.nix"
+    "${vpsadminos}/os/lib/nixos-container/networking.nix"
+  ];
+
+  services.resolved.enable = false;
+}
+
