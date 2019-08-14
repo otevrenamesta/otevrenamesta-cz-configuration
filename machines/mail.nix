c5ce3259270df894e97781cb7e11a461be4bca78
Martin Milata <martin@martinmilata.cz>
2019-08-15 00:09:19 +0200

    mail: allow faking sender address

--- machines/mail.nix
+++ machines/mail.nix
@@ -7,6 +7,7 @@ in
 
 {
   imports = [
+    # updating simple-nixos-mailserver? make sure submissionOptions below stays in sync
     (builtins.fetchTarball {
       url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/v2.2.1/nixos-mailserver-v2.2.1.tar.gz";
       sha256 = "03d49v8qnid9g9rha0wg2z6vic06mhp0b049s3whccn1axvs2zzx";
@@ -151,6 +152,22 @@ in
       sender_canonical_classes = [ "envelope_sender" ];
       recipient_canonical_maps = "tcp:localhost:10002";
       recipient_canonical_classes = [ "envelope_recipient" "header_recipient" ];
+    };
+
+    # disable smtpd_sender_restrictions = reject_sender_login_mismatch that SNM adds
+    # KEEP THIS IN SYNC W/ submissionOptions IN nixos-mailserver/mail-server/postfix.nix
+    submissionOptions = pkgs.lib.mkForce {
+      smtpd_tls_security_level = "encrypt";
+      smtpd_sasl_auth_enable = "yes";
+      smtpd_sasl_type = "dovecot";
+      smtpd_sasl_path = "/run/dovecot2/auth";
+      smtpd_sasl_security_options = "noanonymous";
+      smtpd_sasl_local_domain = "$myhostname";
+      smtpd_client_restrictions = "permit_sasl_authenticated,reject";
+      smtpd_sender_login_maps = "hash:/etc/postfix/vaccounts";
+      #smtpd_sender_restrictions = "reject_sender_login_mismatch";
+      smtpd_recipient_restrictions = "reject_non_fqdn_recipient,reject_unknown_recipient_domain,permit_sasl_authenticated,reject";
+      cleanup_service_name = "submission-header-cleanup";
     };
   };
 }
