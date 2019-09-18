5c606d31440fd43ab564345951cda3f086c7efb5
root <root@mesta-libvirt>
2019-09-18 03:13:04 +0200

    SSH keys

--- machines/proxy.nix
+++ machines/proxy.nix
@@ -18,7 +18,7 @@
   users.users.niap = {
     description = "NIA proxy user (no shell, port forwarding only)";
     shell = "${pkgs.shadow}/bin/nologin";
-    openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [ ms ];
+    openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [ ms ln mm ];
   };
 
   services.nginx = {
