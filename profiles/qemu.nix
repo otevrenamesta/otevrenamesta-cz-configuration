9332b8024f8b197c9b21bcd91064e7957546daa1
Martin Milata <martin@martinmilata.cz>
2020-06-10 18:03:37 +0200

    profiles/qemu: bootloader is on vda now

--- profiles/qemu.nix
+++ profiles/qemu.nix
@@ -6,7 +6,7 @@
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/vda";
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod" ];
   boot.kernelModules = [ ];
