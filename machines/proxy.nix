60089db1ad03c196822a1ff0d24c579f8f139d00
Martin Milata <martin@martinmilata.cz>
2020-07-30 01:30:19 +0200

    proxy: add ukoly.otevrenamesta.cz

--- machines/proxy.nix
+++ machines/proxy.nix
@@ -518,6 +518,23 @@
         };
       };
 
+      "ukoly.otevrenamesta.cz" = {
+        forceSSL = true;
+        enableACME = true;
+
+        locations = {
+          "/" = {
+            proxyPass = "https://[2a01:430:17:1::ffff:10]";
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Real-IP $remote_addr;
+            '';
+          };
+        };
+      };
+
       "ucto.otevrenamesta.cz" = {
         forceSSL = true;
         enableACME = true;
