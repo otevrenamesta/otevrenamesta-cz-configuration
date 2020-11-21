cf41814a06307bcacaf506241df412a312930f05
Martin Milata <martin@martinmilata.cz>
2020-11-22 00:23:03 +0100

    Workaround randomly dying sshd
    https://github.com/NixOS/nixpkgs/issues/99127

--- env.nix
+++ env.nix
@@ -21,6 +21,7 @@ in
   time.timeZone = "Europe/Amsterdam";
   networking.nameservers = [ "172.18.2.10" "172.18.2.11" "208.67.222.222" "208.67.220.220" ];
   services.openssh.enable = true;
+  services.openssh.startWhenNeeded = false; # workaround https://github.com/NixOS/nixpkgs/pull/97989
   nix.useSandbox = true;
   nix.buildCores = 0;
   systemd.tmpfiles.rules = [ "d /tmp 1777 root root 7d" ];
