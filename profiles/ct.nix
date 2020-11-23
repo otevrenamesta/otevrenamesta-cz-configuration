5ac114d02e472b08fd6c8552411ca544390bde5c
Martin Milata <martin@martinmilata.cz>
2020-11-23 20:08:55 +0100

    profiles/ct: update to latest version

--- profiles/ct.nix
+++ profiles/ct.nix
@@ -1,13 +1,15 @@
 { config, pkgs, lib, ... }:
 
 let
-  vpsadminos = builtins.fetchTarball https://github.com/vpsfreecz/vpsadminos/archive/master.tar.gz;
+  vpsadminos = builtins.fetchTarball {
+    url = "https://github.com/vpsfreecz/vpsadminos/archive/a74c43f68034bfc4fe2c5d73ec7533b9d71e1be7.tar.gz";
+    sha256 = "1x59a1v7dph03czaq83hh22kqb393jq0hhz6hng3yy8gvv1ysybr";
+  };
 in
 {
   imports = [
     <nixpkgs/nixos/modules/virtualisation/container-config.nix>
-    "${vpsadminos}/os/lib/nixos-container/build.nix"
-    "${vpsadminos}/os/lib/nixos-container/networking.nix"
+    "${vpsadminos}/os/lib/nixos-container/vpsadminos.nix"
   ];
 
   systemd.services.systemd-udev-trigger.enable = false;
