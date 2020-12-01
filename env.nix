e483684d5bd4ff27da71b577255b68456fdd32e0
Martin Milata <martin@martinmilata.cz>
2020-12-01 04:31:05 +0100

    env: delete defunct DNS servers

--- env.nix
+++ env.nix
@@ -19,7 +19,7 @@ let
 in
 {
   time.timeZone = "Europe/Amsterdam";
-  networking.nameservers = [ "172.18.2.10" "172.18.2.11" "208.67.222.222" "208.67.220.220" ];
+  networking.nameservers = [ "208.67.222.222" "208.67.220.220" ];
   services.openssh.enable = true;
   services.openssh.startWhenNeeded = false; # workaround https://github.com/NixOS/nixpkgs/pull/97989
   nix.useSandbox = true;
